cbf405c2756c05cf649e33079c1448aab7670d5c
ChazzitUP <hamzarezgui.education@gmail.com>
2024-12-01 13:17:04 +0100

    implementing orderline controller

---
+++ services/order/src/main/java/com/hamza/ecommerce/orderline/OrderLineResponse.java
@@ -0,0 +1,7 @@
+package com.hamza.ecommerce.orderline;
+
+public record OrderLineResponse(
+        Integer id,
+        double quantity
+) {
+}
--- services/order/src/main/java/com/hamza/ecommerce/orderline/OrderLineService.java
+++ services/order/src/main/java/com/hamza/ecommerce/orderline/OrderLineService.java
@@ -4,6 +4,9 @@ package com.hamza.ecommerce.orderline;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class OrderLineService {
@@ -12,5 +15,11 @@ public class OrderLineService {
     public Integer saveOrderLine(OrderLineRequest orderLineRequest) {
         var order = mapper.toOrderLine(orderLineRequest);
         return orderLineRepository.save(order).getId();
+    }
+
+    public List<OrderLineResponse> findAllByOrderId(Integer orderId) {
+        return orderLineRepository.findAllByOrderId(orderId).stream()
+                .map(mapper::toOrderLineResponse)
+                .collect(Collectors.toList());
     }
 }
